4d76fb8e3d650f817e8bc7b81d8da3331d0ae1bd
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-27 14:51:22 +0100

    audio preview

--- main.js
+++ main.js
@@ -34,14 +34,17 @@
     var first_track = response.tracks.items[0];
     var title = first_track.name;
     var author = first_track.artists[0].name;
-    var cover_url = first_track.album.images[0].url
+    var cover_url = first_track.album.images[0].url;
+    var preview_url = first_track.preview_url;
 
-    $('.title').text(title);
-    $('.author').text(author);
-    $('.cover').html('<img src="' + cover_url + '">')
+    $('.js-title').text(title);
+    $('.js-author').text(author);
+    $('.js-cover').html('<img src="' + cover_url + '">');
+    $('.js-audio').attr('src', preview_url);
     console.log(title);
     console.log(author);
     console.log(cover_url);
+    console.log(preview_url);
   }
 
 
